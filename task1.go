84a38839d8a7ae6b6e92f4dabb18a71a17234ccb
Berkut Ksu <76900160+BerkutKsu@users.noreply.github.com>
2026-03-02 17:59:12 +0200

    Update task1.go

--- task1.go
+++ task1.go
@@ -11,16 +11,25 @@ func main() {
 	ch := make(chan bool)
 	ch2 := make(chan bool)
 	ch3 := make(chan bool)
+
+	start := make(chan bool) // Добавим еще один канал start для синхронизации запуска горутин
 	go func() {
+		<-start
 		ch <- true
 	}()
 	go func() {
+		<-start
 		ch2 <- true
 	}()
 	go func() {
+		<-start
 		ch3 <- true
 	}()
 
+	go func() {
+		start <- true
+	}() // Запускаем отдельную горутину, которая отправляет сигнал один раз,
+	// чтобы только первая горутина произвела запись в свой канал
 	select {
 	case <-ch:
 		fmt.Printf("val from ch")
