26336844a545db46d7e7ea1899f9973fce39897c
Berkut Ksu <76900160+BerkutKsu@users.noreply.github.com>
2026-03-03 13:44:44 +0200

    Create task4.go

---
+++ task4.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.NewSource(time.Now().UnixNano()) //для составления рандомных чисел. больше тебе знать не нужно
+}
+
+// time.Sleep использовать нельзя. это будет не валидным ответом на собеседовании
+// Есть функция unpredictableFunc, работающая неопределенно долго и возвращающая число.
+// Её тело нельзя изменять (представим, что внутри сетевой запрос).
+func unpredictableFunc() int64 {
+	rnd := rand.Int63n(5000) //рандомное число
+	time.Sleep(time.Duration(rnd) * time.Millisecond)
+
+	return rnd
+}
+
+// Нужно изменить функцию обертку predictableFunc, которая будет работать с заданным таймаутом (например, 1 секунду).
+// Если "длинная" функция отработала за это время - отлично, возвращаем результат.
+// Если нет - возвращаем ошибку. Результат работы в этом случае нам не важен.
+//// т.к. в условии задачи написано, но нужно возвращать ошибку, в возвращаемые значения функции был добавлен тип error
+func predictableFunc() (int64, error) {
+  ctx := context.Context(context.Background())
+	withDeadline, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second))
+	defer cancel()
+	randomResult := unpredictableFunc()
+	select {
+	case <-withDeadline.Done():
+		return 0, errors.New("context deadline exceeded")
+	default:
+		return randomResult, nil
+	}
+}
+
+func main() {
+	fmt.Println("started")
+
+	fmt.Println(predictableFunc())
+}
